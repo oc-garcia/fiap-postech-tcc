11d219c69575b2a1c2e0fd907f7da61d0c55d7c3
oc-garcia <octaviogarcia1988@gmail.com>
2025-01-24 16:17:26 -0300

    Add TeachingContentForm component and define interfaces for content management

---
+++ src/app/teste/page.tsx
@@ -0,0 +1,10 @@
+import { TeachingContentForm } from "@/components/TeachingContentForm/TeachingContentForm";
+import React from "react";
+
+export const Teste = () => {
+  return (
+    <div>
+      <TeachingContentForm />
+    </div>
+  );
+};
